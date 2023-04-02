2d7f77f2ab161a55fda7d5916bba31d9df6838e9
tatata05 <t.t.syukatsu.t.t@gmail.com>
2023-04-02 10:23:34 +0900

    typeカラムにenumの追加

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,4 +2,15 @@ class Notification < ApplicationRecord
   belongs_to :employee, optional: true
   belongs_to :shift, optional: true
   belongs_to :absence, optional: true
+
+  enum type: { 
+    unapplied: 0, 
+    application_shift: 1, 
+    approval_pending_shift: 2,
+    application_absence: 3,
+    approval_pending_absence: 4,
+    approval_shift: 5,
+    rejected_shift: 6,
+    approval_absence: 7,
+    approval_absence: 8 }
 end
