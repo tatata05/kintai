9df79af26a95360eece0d258ae600eb71a4805c0
tatata05 <t.t.syukatsu.t.t@gmail.com>
2023-05-04 19:21:42 +0900

    各リンクの遷移先修正

--- app/controllers/employees/sessions_controller.rb
+++ app/controllers/employees/sessions_controller.rb
@@ -3,6 +3,10 @@
 class Employees::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  def after_sign_in_path_for(resource)
+    employee_shifts_path
+  end
+
   # GET /resource/sign_in
   # def new
   #   super
