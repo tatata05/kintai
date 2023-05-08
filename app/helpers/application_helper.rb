7a88852a1e2a009d4b63724d98cc936e1ae06be4
tatata05 <t.t.syukatsu.t.t@gmail.com>
2023-05-08 10:57:22 +0900

    notification.typeの修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,9 +7,9 @@ module ApplicationHelper
   def display_admin_notification(notification)
     case notification.kind
     when "application"
-      render "admin/notifications/application", notification: notification, type: notification.type
+      render "admin/notifications/application", notification: notification
     when "approval_pending"
-      render "admin/notifications/approval_pending", notification: notification, type: notification.type
+      render "admin/notifications/approval_pending", notification: notification
     when "unapplied"
       render "admin/notifications/unapplied", notification: notification
     end
@@ -18,9 +18,9 @@ module ApplicationHelper
   def display_employee_notification(notification)
     case notification.kind
     when "approval"
-      render "employee/notifications/approval", notification: notification, type: notification.type
+      render "employee/notifications/approval", notification: notification
     when "rejected"
-      render "employee/notifications/rejected", notification: notification, type: notification.type
+      render "employee/notifications/rejected", notification: notification
     when "unapplied"
       render "employee/notifications/unapplied", notification: notification
     end
