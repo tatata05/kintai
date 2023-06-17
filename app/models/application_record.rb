62e712dcba64a9bffc495c68f1bda01d739fe1b1
tatata05 <t.t.syukatsu.t.t@gmail.com>
2023-06-17 15:55:58 +0900

    検索機能追加

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.search(search)
+    return self.all unless search
+    self.where(['name LIKE ?', "%#{search}%"])
+  end
 end
